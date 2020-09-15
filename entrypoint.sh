f0ebd4d5fb39adc070a7d01eaba475e3b946a41e
Pushkar Acharya <pacharya@platform9.com>
2020-09-15 14:47:28 -0700

    Ignore swap enabled in PMK

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 mkdir -p /var/lib/docker/network/files
-# /usr/local/bin/docker-entrypoint.sh "$@" > /var/log/docker-entrypoint.log & 2>&1
+
 setsid /dind dockerd > /var/log/dockerd.log & 2>&1
+
 setsid /get_cli.sh &
+
 exec /usr/sbin/init
